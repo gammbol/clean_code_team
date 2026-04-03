0684fab2120a0ed4b32200c28f5af9f68d36d77a
Ver-yu <udina-vera-vera@yandex.ru>
2026-04-03 11:06:25 +0300

    Update LoggingDecorator.cs

--- decorator/decorator/Decorators/LoggingDecorator.cs
+++ decorator/decorator/Decorators/LoggingDecorator.cs
@@ -1,6 +1,5 @@
-﻿// Конкретный декоратор
-// Добавляет функциональность ЛОГИРОВАНИЯ запросов и ответов.
-// Логирует: что пришло, сколько времени обрабатывалось, что ушло.
+// Конкретный декоратор
+// логирование запросов и ответов
 
 using HttpDecoratorSystem.Interfaces;
 using HttpDecoratorSystem.Models;
@@ -11,10 +10,8 @@ namespace HttpDecoratorSystem.Decorators
 {
     public class LoggingDecorator : RequestHandlerDecorator
     {
-        // Сервис логирования (интерфейс, чтобы можно было подменить реализацию)
         private readonly ILogger _logger;
 
-        // Конструктор: принимает следующий обработчик и логгер
         public LoggingDecorator(IHttpRequestHandler innerHandler, ILogger logger)
             : base(innerHandler)
         {
@@ -22,29 +19,23 @@ namespace HttpDecoratorSystem.Decorators
         }
 
 
-        /// Переопределяем метод обработки, добавляя логирование
-
+        // + логирование
         public override async Task<HttpResponse> HandleAsync(HttpRequest request, CancellationToken ct = default)
         {
-            // Запускаем секундомер для замера времени выполнения
+            // секундомер для замера времени выполнения
             var stopwatch = Stopwatch.StartNew();
 
-            // Логирование до обработки 
-            _logger.LogInfo($"[LOG] Запрос: {request.Method} {request.Url}");
-            _logger.LogInfo($"[LOG] User: {request.UserId ?? "anonymous"}");
-            _logger.LogInfo($"[LOG] Headers: {request.Headers.Count}");
+            _logger.LogInfo($"(дек лог): Запрос {request.Method} {request.Url}");
+            _logger.LogInfo($"(дек лог):  User {request.UserId ?? "anonymous"}");
+            _logger.LogInfo($"(дек лог): Headers {request.Headers.Count}");
 
-            // Вызываем следующий обработчик в цепочке
-            // Это может быть другой декоратор или базовый обработчик
             var response = await _innerHandler.HandleAsync(request, ct);
 
-            // Останавливаем секундомер
             stopwatch.Stop();
 
-            // Логирование после обработки 
-            _logger.LogInfo($"[LOG] Ответ: {response.StatusCode} ({stopwatch.ElapsedMilliseconds}ms)");
+            // логирование после обработки 
+            _logger.LogInfo($"(дек лог): Ответ: {response.StatusCode} ({stopwatch.ElapsedMilliseconds}ms)");
 
-            // Сохраняем время выполнения в ответ (для мониторинга)
             response.ExecutionTime = stopwatch.Elapsed;
             return response;
         }
